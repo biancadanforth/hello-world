f7f9bdd135abb95243fda5c776af922628043a25
Bianca <bianca.scura@gmail.com>
2016-09-15 12:34:13 -0700

    Search box styles added
    Search box styles added, though not complete

--- To-Do-List/js/script.js
+++ To-Do-List/js/script.js
@@ -1,1 +1,18 @@
 // To Do Project JavaScript
+
+var searchBar = document.getElementById("search-bar");
+var searchIcon = document.getElementById("search-icon");
+var searchTextBox = document.getElementById("search-text-box");
+
+function showSearchBar() {
+  searchBar.style.display="inline";
+  searchIcon.style.display="none";
+}
+
+function hideSearchBar() {
+  searchBar.style.display="none";
+  searchIcon.style.display="inline-block";
+}
+
+searchIcon.onclick = showSearchBar;
+searchTextBox.onblur = hideSearchBar;
