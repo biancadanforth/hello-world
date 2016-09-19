d0e42cd80d263d27a9ba85d5537017e7f6612a3c
Bianca <bianca.scura@gmail.com>
2016-09-19 14:00:09 -0700

    Add files via upload

--- InteractiveSearchBar/script.js
+++ InteractiveSearchBar/script.js
@@ -1,18 +1,22 @@
-// To Do Project JavaScript
+// document.getElementsByClassName(...) returns an array-like object of all child elements which have all of the given class names. Just choose the first element in the array, array[0].
+var searchIcon = document.getElementsByClassName("search-icon")[0];
+var searchButton = document.getElementsByClassName("search-button")[0];
+var searchBox = document.getElementsByClassName("search-box")[0];
 
-var searchBar = document.getElementById("search-bar");
-var searchIcon = document.getElementById("search-icon");
-var searchTextBox = document.getElementById("search-text-box");
+function toggleClass() {
+  searchIcon.classList.toggle("search-icon");
+  searchIcon.classList.toggle("js-search-icon-active");
+  searchButton.classList.toggle("search-button");
+  searchButton.classList.toggle("js-search-button-active");
+  searchBox.classList.toggle("search-box");
+  searchBox.classList.toggle("js-search-box-active");
+  if (searchBox.classList.contains("js-search-box-active")) {
+    searchBox.focus();
+  }
+};
 
-function showSearchBar() {
-  searchBar.style.display="inline";
-  searchIcon.style.display="none";
-}
+// activate CSS3 transitions!
+searchIcon.onclick = toggleClass;
 
-function hideSearchBar() {
-  searchBar.style.display="none";
-  searchIcon.style.display="inline-block";
-}
-
-searchIcon.onclick = showSearchBar;
-searchTextBox.onblur = hideSearchBar;
+// return to initial state
+searchBox.onblur = toggleClass;
