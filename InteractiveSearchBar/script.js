01c7a00044f67453319e7e01ec7c7fdb2070343c
Bianca <bianca.scura@gmail.com>
2016-09-23 10:06:11 -0700

    Removed notes to self

--- InteractiveSearchBar/script.js
+++ InteractiveSearchBar/script.js
@@ -1,12 +1,8 @@
-/* document.getElementsByClassName(...) returns an array-like object of all child elements which have all of the given class names. Just choose the first element in the array, array[0]. */
 var searchIcon = document.getElementsByClassName("search-icon")[0];
 var searchButton = document.getElementsByClassName("search-button")[0];
 var searchBox = document.getElementsByClassName("search-box")[0];
 
 function toggleClass() {
-// IE11 bug workaround to toggle class names for search icon svg, since element.classList doesn't work on SVGs. *sigh*
-/*   searchIcon.classList.toggle("search-icon");
-  searchIcon.classList.toggle("js-search-icon-active"); */
   searchButton.classList.toggle("search-button");
   searchButton.classList.toggle("js-search-button-active");
   searchBox.classList.toggle("js-search-box-active");
@@ -15,7 +11,7 @@ function toggleClass() {
   }
 }
 
-// activate CSS3 transitions!
+// activate CSS3 transitions
 searchButton.onclick = toggleClass;
 
 // return to initial state
