4a49c5f272b74cc38f69f638fcffdc6f5323a5d4
Bianca <bianca.scura@gmail.com>
2016-09-23 10:02:48 -0700

    Modified script and CSS to accommodate IE11
    IE11 element.classList on svgs does not work. Replaced with descendant selector on toggled parent element to svg, <button>.

--- InteractiveSearchBar/script.js
+++ InteractiveSearchBar/script.js
@@ -1,21 +1,23 @@
-// document.getElementsByClassName(...) returns an array-like object of all child elements which have all of the given class names. Just choose the first element in the array, array[0].
+/* document.getElementsByClassName(...) returns an array-like object of all child elements which have all of the given class names. Just choose the first element in the array, array[0]. */
 var searchIcon = document.getElementsByClassName("search-icon")[0];
 var searchButton = document.getElementsByClassName("search-button")[0];
 var searchBox = document.getElementsByClassName("search-box")[0];
 
 function toggleClass() {
-  searchIcon.classList.toggle("search-icon");
-  searchIcon.classList.toggle("js-search-icon-active");
+// IE11 bug workaround to toggle class names for search icon svg, since element.classList doesn't work on SVGs. *sigh*
+/*   searchIcon.classList.toggle("search-icon");
+  searchIcon.classList.toggle("js-search-icon-active"); */
   searchButton.classList.toggle("search-button");
   searchButton.classList.toggle("js-search-button-active");
   searchBox.classList.toggle("js-search-box-active");
   if (searchBox.classList.contains("js-search-box-active")) {
     searchBox.focus();
   }
-};
+}
 
 // activate CSS3 transitions!
-searchIcon.onclick = toggleClass;
+searchButton.onclick = toggleClass;
 
 // return to initial state
 searchBox.onblur = toggleClass;
+
