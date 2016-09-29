26d848cecdcd1bb0671a93ecdb369b1a4b0aa31a
Bianca <bianca.scura@gmail.com>
2016-09-29 15:37:18 -0700

    Added 'delete list item' functionality
    Now the user can add and delete as many list items as they want.

--- To-Do-List/modules/list-item/script.js
+++ To-Do-List/modules/list-item/script.js
@@ -1,4 +1,4 @@
-// !----------ADDING A TASK----------!
+// !----------ADDING A TASK----------! reference: http://jsfiddle.net/g79ssyqv/12/
 var row = 0;
 var addTaskContainer = document.getElementById("add-task-container");
 var listContainer = document.getElementById("list-container");
@@ -6,11 +6,14 @@ var listContainer = document.getElementById("list-container");
 addTaskContainer.addEventListener("click", addTask);
 
 function addTask() {
-  addTaskContainer.style.borderTopWidth = "0";
   //create a new, unique list-item-container div
   var containerNode = document.createElement("div");
   containerNode.setAttribute("id", "list-item-container-" + row);
   containerNode.setAttribute("class", "list-item-container");
+  // remove border around containerNode permanently when at least one instance of it is present.
+  if (row >= 0) {
+    containerNode.style.borderBottom = "0";
+  }
   // put the new element in the DOM at a specific location
   listContainer.appendChild(containerNode);
   
@@ -47,18 +50,29 @@ function addTask() {
   // add node to DOM at a particular location
   containerNode.appendChild(inputNode);
   
-  //create a new, unique delete icon inside list-item-container div
+  // create a new, unique anchor tag to wrap the delete icon svg inside the list-item-container div
+  var svgWrapper = document.createElement("a");
+  containerNode.appendChild(svgWrapper);
+  
+  //create a new, unique delete icon inside anchor tag
  //This requires obtaining the svg XML element from the web server
   var deleteIcon = new XMLHttpRequest();
   deleteIcon.open("GET", "/images/delete-icon.svg", true);
   deleteIcon.overrideMimeType("image/svg+xml");
   deleteIcon.onreadystatechange = function() {
     if (deleteIcon.readyState === XMLHttpRequest.DONE && deleteIcon.status === 200) {
-      containerNode.appendChild(deleteIcon.responseXML.documentElement);
+      svgWrapper.appendChild(deleteIcon.responseXML.documentElement);
     }
   }
   deleteIcon.send();
   //Cannot [at least easily] add classes/ids in JS on an XML resource; just target parent class and use descendant selector for styling: e.g. .parent svg {..}.
   
+  // DELETE A TASK---------- reference: http://jsfiddle.net/g79ssyqv/12/
+  svgWrapper.setAttribute("onclick", "deleteTask(" + row.toString() + ");");
+  
   row++;
 }
+
+function deleteTask(idNum) {
+  document.getElementById("list-item-container-" + idNum).remove();
+  };
